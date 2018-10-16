2861d90119bf0f43cfc17df2eeaf161612207b12
Patrick Marchand <pmarchand+git@evolix.ca>
2018-10-16 14:35:23 -0400

    Fixes optional group creation
    The optional group creation flag in scripts/web-add.sh was never used.

--- scripts/web-add.sh
+++ scripts/web-add.sh
@@ -259,7 +259,7 @@ create_www_account() {
     ############################################################################
 
 
-    /usr/sbin/adduser --gecos "User $in_login" --disabled-password "$in_login" --shell /bin/bash $OPT_UID $OPT_UID_ARG --force-badname --home "$HOME_DIR_USER" >/dev/null
+    /usr/sbin/adduser --gecos "User $in_login" --disabled-password "$in_login" --shell /bin/bash "$OPT_UID" "$OPT_UID_ARG" "$OPT_GID" "$OPT_GID_ARG"  --force-badname --home "$HOME_DIR_USER" >/dev/null
     [ -z "$in_sshkey" ] && echo "$in_login:$in_passwd" | chpasswd --md5
     [ -z "$in_sshkey" ] || [ -n "$HOME_DIR_USER" ] && mkdir "$HOME_DIR_USER/.ssh" && echo "$in_sshkey" > "$HOME_DIR_USER/.ssh/authorized_keys" \
         && chmod -R u=rwX,g=,o= "$HOME_DIR_USER/.ssh/authorized_keys" && chown -R "$in_login":"$in_login" "$HOME_DIR_USER/.ssh"
