Evolix/evoadmin-web

commit f2f115608aff56e03236114f79e3d7de2a8eda21

Dump & remove user crontab on account deletion
closes #37

scripts/web-add.sh
@@ -729,6 +729,10 @@ op_del() {
     fi
 
     set -x
+    # Crontab dump needs to be done **before** user deletion
+    crontab -l -u "$login" &> /home/$login/crontab-$(date '+%Y%m%d-%H%M%S').bak
+    crontab -r -u "$login"
+
     if [ "$WEB_SERVER" == "apache" ]; then
         if id www-"$login" &> /dev/null; then
             userdel -f www-"$login"

commit 93629b51fb07694469f468981e3545b9a898b03e

Fix bug introduced in f2f115608a (op del fails when user has no crontab)

scripts/web-add.sh
@@ -735,8 +735,10 @@ op_del() {
 
     set -x
     # Crontab dump needs to be done **before** user deletion
-    crontab -l -u "$login" &> /home/$login/crontab-$(date '+%Y%m%d-%H%M%S').bak
-    crontab -r -u "$login"
+    if crontab -l -u "$login"; then
+        crontab -l -u "$login" &> /home/$login/crontab-$(date '+%Y%m%d-%H%M%S').bak
+        crontab -r -u "$login"
+    fi
 
     if [ "$WEB_SERVER" == "apache" ]; then
         if id www-"$login" &> /dev/null; then