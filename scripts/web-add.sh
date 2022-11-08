Evolix/evoadmin-web

commit 9913579bb08f053cedbdaf3476bce0ac17e76e82

Fix drop database when contains dash

scripts/web-add.sh
@@ -814,7 +814,7 @@ op_del() {
 
     if [ -n "$dbname" ]; then
         set -x
-        echo "DROP DATABASE $dbname; delete from mysql.user where user='$login' ; FLUSH PRIVILEGES;" | mysql $MYSQL_OPTS
+        echo "DROP DATABASE \`$dbname\`; delete from mysql.user where user=\`$login\`; FLUSH PRIVILEGES;" | mysql $MYSQL_OPTS
         set +x
     fi
 }

commit 67d0c45d79e63dc34f23edda08ac0aff620c8b0f

Fix bug introduced in 9913579bb0 (MariaDB does not support backtics in where clause).

scripts/web-add.sh
@@ -814,7 +814,7 @@ op_del() {
 
     if [ -n "$dbname" ]; then
         set -x
-        echo "DROP DATABASE \`$dbname\`; delete from mysql.user where user=\`$login\`; FLUSH PRIVILEGES;" | mysql $MYSQL_OPTS
+        echo "DROP DATABASE \`$dbname\`; delete from mysql.user where user='$login'; FLUSH PRIVILEGES;" | mysql $MYSQL_OPTS
         set +x
     fi
 }