a348433c01c52fed79ee22bf13a322d3a97d0ddf
Alexis Ben Miloud--Josselin <abenmiloud+git@evolix.fr>
2025-05-27 11:49:35 +0200

    web-add: s/\t/    /

--- scripts/web-add.sh
+++ scripts/web-add.sh
@@ -109,7 +109,7 @@ list-vhost LOGIN
 
 check-vhosts -f
 
-	List suggested changes to vhosts, apply fixes with -f
+    List suggested changes to vhosts, apply fixes with -f
 
 add-alias VHOST ALIAS
 
@@ -279,7 +279,7 @@ create_www_account() {
     ############################################################################
 
     if [ -f $PRE_LOCAL_SCRIPT ]; then
-    	# shellcheck source=/usr/share/scripts/evoadmin/web-add.pre-local.sh
+        # shellcheck source=/usr/share/scripts/evoadmin/web-add.pre-local.sh
         source $PRE_LOCAL_SCRIPT
     fi
 
@@ -298,16 +298,16 @@ create_www_account() {
         return 1
     fi
 
-	# Create user and force UID / GID if specified
+    # Create user and force UID / GID if specified
     /usr/sbin/adduser \
-    	--gecos "User $in_login" \
-    	--disabled-password \
-    	"$in_login" \
-    	--shell /bin/bash \
-    	${in_uid:+'--uid' "$in_uid"} \
-    	${in_gid:+'--gid' "$in_gid"} \
-    	--force-badname \
-    	--home "$HOME_DIR_USER" >/dev/null
+        --gecos "User $in_login" \
+        --disabled-password \
+        "$in_login" \
+        --shell /bin/bash \
+        ${in_uid:+'--uid' "$in_uid"} \
+        ${in_gid:+'--gid' "$in_gid"} \
+        --force-badname \
+        --home "$HOME_DIR_USER" >/dev/null
 
     [ -z "$in_sshkey" ] \
     && echo "$in_login:$in_passwd" | chpasswd
@@ -316,7 +316,7 @@ create_www_account() {
     || [ -n "$HOME_DIR_USER" ] \
     && mkdir "$HOME_DIR_USER/.ssh" \
     && echo "$in_sshkey" > "$HOME_DIR_USER/.ssh/authorized_keys" \
-	&& chmod -R u=rwX,g=,o= "$HOME_DIR_USER/.ssh/authorized_keys" \
+    && chmod -R u=rwX,g=,o= "$HOME_DIR_USER/.ssh/authorized_keys" \
     && chown -R "$in_login":"$in_login" "$HOME_DIR_USER/.ssh"
 
     # Create www user and force UID if specified
@@ -535,7 +535,7 @@ EOT
     ############################################################################
 
     if [ -f $LOCAL_SCRIPT ]; then
-    	# shellcheck source=/usr/share/scripts/evoadmin/web-add.local.sh
+        # shellcheck source=/usr/share/scripts/evoadmin/web-add.local.sh
         source $LOCAL_SCRIPT
     fi
 
@@ -1317,34 +1317,34 @@ op_checkvhosts() {
     ln_vhosts_dir="$(sed 's/available/enabled/' <<< "$VHOST_PATH")"
     non_ln_vhosts="$(find "$ln_vhosts_dir"/* ! -type l)"
 
-	while getopts f opt; do
-		case "$opt" in
-		f)
-			apply=1
-			;;
-		?)
-			usage
-			exit 1
-			;;
-		esac
-	done
+    while getopts f opt; do
+        case "$opt" in
+        f)
+            apply=1
+            ;;
+        ?)
+            usare
+            exit 1
+            ;;
+        esac
+    done
 
-	for ln_path in $non_ln_vhosts
+    for ln_path in $non_ln_vhosts
     do
-		vhost_name=$(basename "$ln_path")
-		fix_conf="mv $ln_path $VHOST_PATH/$vhost_name"
-		fix_ln="a2ensite ${vhost_name}.conf"
+        vhost_name=$(basename "$ln_path")
+        fix_conf="mv $ln_path $VHOST_PATH/$vhost_name"
+        fix_ln="a2ensite ${vhost_name}.conf"
 
-		if [[ -z "$apply" ]]; then
-			echo "Suggested fixes for $vhost_name:"
-			echo "diff $ln_path $VHOST_PATH/$vhost_name"
-			echo "$fix_conf"
-			echo "$fix_ln"
-		else
-			$fix_conf
-			$fix_ln
-		fi
-	done
+        if [[ -z "$apply" ]]; then
+            echo "Suggested fixes for $vhost_name:"
+            echo "diff $ln_path $VHOST_PATH/$vhost_name"
+            echo "$fix_conf"
+            echo "$fix_ln"
+        else
+            $fix_conf
+            $fix_ln
+        fi
+    done
 }
 
 # Return web-add.sh version
